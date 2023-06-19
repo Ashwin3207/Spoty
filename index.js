73dcaf395fb660f412cfd26603c4b742d5daa303
Ashwin3207 <58184287+Ashwin3207@users.noreply.github.com>
2023-06-19 20:43:35 +0530

    changes

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 function perfect()
-{document.getElementById('ss').innerHTML='<h3>Perfect</h3>';
-    document.getElementById('player').innerHTML='<audio autoplay controls id="play"><source id="sr" src="perfect.mp3" type="audio/ogg"></audio>';
+{
 }
 function suzume()
-{   document.getElementById('ss').innerHTML='<h3>Suzume</h3>'
+{   
+}
+let a;
+function play(a)
+{   if(a==1)
+    {document.getElementById('ss').innerHTML='<h3>Perfect</h3>';
+    document.getElementById('player').innerHTML='<audio autoplay controls id="play"><source id="sr" src="perfect.mp3" type="audio/ogg"></audio>';
+}
+    if(a==2)
+    
+    {document.getElementById('ss').innerHTML='<h3>Suzume</h3>'
     document.getElementById('player').innerHTML='<audio autoplay controls id="play"><source id="sr" src="suzume.mp3" type="audio/ogg"></audio>';
+    }
+    if(a==3)
+    {
+    document.getElementById('ss').innerHTML='<h3 height="30px">Champagne Problems</h3>';
+    document.getElementById('player').innerHTML='<audio autoplay controls id="play"><source id="sr" src="champagne.mp3" type="audio/ogg"></audio>';
+    }
 }
 
 document.getElementById("1").addEventListener("click",perfect);
