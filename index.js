4ed5fdd4e5ceaebe165b348171c1aada3638bfb6
Ashwin3207 <58184287+Ashwin3207@users.noreply.github.com>
2023-06-25 16:45:38 +0530

    fixed name

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 let a;
 function play(a) {
     if (a == 1) {
-        document.getElementById('ss').innerHTML = '<h3>Perfect</h3>';
+        document.getElementById('ss').innerHTML = '<p>Perfect</p>';
         document.getElementById('player').innerHTML = '<audio autoplay controls id="play"><source id="sr" src="perfect.mp3" type="audio/ogg"></audio>';
        
     }
     if (a == 2) {
-        document.getElementById('ss').innerHTML = '<h3>Suzume</h3>';
+        document.getElementById('ss').innerHTML = '<p>Suzume</p>';
         document.getElementById('player').innerHTML = '<audio autoplay controls id="play"><source id="sr" src="suzume.mp3" type="audio/ogg"></audio>';
       
     }
     if (a == 3) {
-        document.getElementById('ss').innerHTML = '<h3 height="30px">Champagne Problems</h3>';
+        document.getElementById('ss').innerHTML = '<p height="30px">Champagne Problems</p>';
         document.getElementById('player').innerHTML = '<audio autoplay controls id="play"><source id="sr" src="champagne.mp3" type="audio/ogg"></audio>';
         
     }
